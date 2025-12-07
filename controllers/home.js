f3587110ca37b922177e2898d8a5a00b4a741cf2
Etay Sosman <121424319+etaysosman@users.noreply.github.com>
2025-12-07 17:31:48 +0200

    Delete controllers directory

--- controllers/home.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-exports.getHome = (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
-};
-
-exports.getAbout = (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/about.html'));
-};
