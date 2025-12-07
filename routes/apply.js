ab4414418d813e3e5888b1dcf2a1a281a1e15ff2
Etay Sosman <121424319+etaysosman@users.noreply.github.com>
2025-12-07 17:31:31 +0200

    Delete routes directory

--- routes/apply.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const applyController = require('../controllers/apply');
-
-const router = express.Router();
-
-router.get('/apply/:id', applyController.getApplyForm);
-router.post('/save-apply', applyController.saveApplication);
-
-module.exports = router;
