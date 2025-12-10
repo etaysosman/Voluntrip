7cc2234e97dcbc0826f067f242eb5e4481fba24a
Etay Sosman <121424319+etaysosman@users.noreply.github.com>
2025-12-10 09:22:28 +0200

    Add urlencoded middleware to users routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const usersController = require('../controllers/users');
-
 const router = express.Router();
+router.use(express.urlencoded({extended:false}));
 
 //register
 router.get('/register', usersController.getRegisterPage);
@@ -10,5 +10,6 @@ router.post('/register', usersController.registerUser);
 //login
 router.get('/login', usersController.getLoginPage);
 router.post('/login', usersController.loginUser);
+
 
 module.exports = router;
