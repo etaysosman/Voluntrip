a549c071cac2266e162d0486e4b769fb5baa8eb4
Etay Sosman <121424319+etaysosman@users.noreply.github.com>
2026-02-18 19:45:06 +0200

    Delete public/js directory

--- public/js/successful-msg.js
+++
@@ -1,9 +0,0 @@
-   
-   const form = document.getElementById('form');
-
-  if (form) {
-      form.addEventListener('submit', function() {
-      alert("Everything was sent Successfuly! Thank you.");
-      this.submit();
-    });
-  }
