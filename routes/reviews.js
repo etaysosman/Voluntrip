2fea5447decd35f2429618c56804787b5d252d39
Etay Sosman <121424319+etaysosman@users.noreply.github.com>
2026-02-18 19:43:39 +0200

    Delete routes directory

--- routes/reviews.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const reviewsController = require('../controllers/reviews');
-
-const router = express.Router();
-
-router.get('/reviews', reviewsController.getReviews);
-router.post('/reviews', reviewsController.postReview);
-
-module.exports = router;
