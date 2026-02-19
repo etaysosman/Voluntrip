929a3a5e26955998ee0010d5be6eefef788e4c74
Etay Sosman <121424319+etaysosman@users.noreply.github.com>
2026-02-19 23:01:09 +0200

    Delete util directory

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const mysql=require('mysql2');
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'voluntrip',
-    password:'qwerty123'
-});
-module.exports=pool.promise();
