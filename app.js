29ee172ae450103b6b7a1cc449058dc20c26c020
Etay Sosman <121424319+etaysosman@users.noreply.github.com>
2026-02-20 22:06:52 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-
 const path = require('path');
-
 const homeRoutes = require('./routes/home');
 const reviewsRoutes = require('./routes/reviews');
 const applyRoutes = require('./routes/apply');
@@ -12,27 +10,27 @@ const app = express();
 
 app.use(session({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized:false,
+    saveUninitialized: false,
     resave: false
 }));
 
-//Tell Express to use EJS
+// Tell Express to use EJS
 app.set('views', path.join(__dirname, 'views'));
-
+app.set('view engine', 'ejs');
 
 // Middleware to parse URL-encoded bodies
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-// Static files (css, images) 
+// Static files (css, images, js, media) 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'ejs');
+
+// Routes
 app.use(usersRoutes);
 app.use(homeRoutes);
 app.use(reviewsRoutes);
-app.use(applyRoutes);
-
+app.use(applyRoutes); // this one contains 404 handeling
 
 app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+    console.log('Server running on http://localhost:3000');
 });
