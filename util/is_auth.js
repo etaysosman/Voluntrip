d37b696608e6ab0c794eaf4802f554d3a70eaad0
Etay Sosman <121424319+etaysosman@users.noreply.github.com>
2026-02-21 17:12:20 +0200

    Delete util directory

--- util/is_auth.js
+++
@@ -1,9 +0,0 @@
-// Middleware to protect routes: redirects to login if the user is not authenticated
-module.exports = (req, res, next) => {
-    if (!req.session.isLoggedIn) {
-        return res.redirect('/login');
-    }
-
-    // Proceed to the next middleware or route handler
-    next();
-};
